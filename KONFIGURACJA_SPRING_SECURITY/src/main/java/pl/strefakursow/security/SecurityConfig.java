df62b1d7bc2ae96dec5fc04b2c9fb14aff0637a7
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-03-17 22:20:00 +0100

    Bezpieczne aplikacje JAVA - Konfiguracja Form login

--- KONFIGURACJA_SPRING_SECURITY/src/main/java/pl/strefakursow/security/SecurityConfig.java
+++ KONFIGURACJA_SPRING_SECURITY/src/main/java/pl/strefakursow/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package pl.strefakursow.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,15 +17,34 @@ import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
+    @Order(0)
+    @Configuration
+    public static class HttpBasicConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/secured-basic")
+                    .authorizeRequests().anyRequest()
+                    .authenticated()
+                    .and().httpBasic()
+                    .and().csrf().disable();
+        }
+    }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")
-                .authorizeRequests().anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+    @Order(1)
+    @Configuration
+    public static class FormLoginConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/secured-form/**")
+                    .authorizeRequests()
+                    .antMatchers("/secured-form/login")
+                    .permitAll()
+                    .anyRequest().authenticated()
+                    .and().formLogin().loginPage("/secured-form/login")
+                    .loginProcessingUrl("/secured-form/login")
+                    .and().csrf().disable();
+        }
     }
 
     @Configuration
